ffccd4bb14644b5befa1d6bce4216273a124f6f7
arthu <arthurmarcelosantanaalves@hotmail.com>
2020-05-06 15:41:12 -0300

    FEAT: create unordered list

--- src/main.js
+++ src/main.js
@@ -1,17 +1,29 @@
 const fs = require('fs')
 const p = require('./modules/html/p')
 const h = require('./modules/html/h')
+const list = require('./modules/html/list')
 const table = require('./modules/html/table')
-const { Document, Packer } = require('docx')
+const { Document, Packer, Paragraph, TextRun } = require('docx')
 const getBody = require('./modules/getBody')
 const getTags = require('./modules/getTags')
 
 fs.readFile('./test.html', async (err, data) => {
 
 	let doc = []
+	doc.push(new Paragraph({
+		text: "EEAASDASDA S",
+		bullet: {
+			level: 1,
+		},
+	}))
 	const body = await getBody(data.toString())
 	const tags = await getTags(body)
-	let tagTable = []
+	let tableTag = []
+	let listTag = {
+		tags: [],
+		countOpen: 0,
+		countClose: 0
+	}
 	tags.forEach(tag => {
 		switch (tag['name']) {
 			case 'p':
@@ -26,17 +38,34 @@ fs.readFile('./test.html', async (err, data) => {
 			case 'h3':
 				doc.push(h(tag['content'], 3))
 				break
-			case 'tr':
+			case 'ul':
+				listTag.countOpen++
+				listTag.tags.push(tag)
+				break
+			case 'li':
+				listTag.tags.push(tag)
+				break
+			case '/ul':
+				listTag.tags.push(tag)
+				listTag.countClose++
+				if(listTag.countOpen === listTag.countClose) {
+					doc.push(...list(listTag.tags))
+					listTag.countOpen = 0
+					listTag.countClose = 0
+					listTag.tags = []
+				}
+				break
 			case 'table':
+			case 'tr':
 			case 'th':
 			case 'td':
 			case '/tr':
-				tagTable.push(tag)
+				tableTag.push(tag)
 				break
 			case '/table':
-				tagTable.push(tag)
-				doc.push(table(tagTable))
-				tagTable = []
+				tableTag.push(tag)
+				doc.push(table(tableTag))
+				tableTag = []
 				break
 		}
 	})
---
+++ src/modules/html/list.js
@@ -0,0 +1,24 @@
+const { Paragraph } = require('docx')
+module.exports = (tags) => {
+    let list = []
+    let level = 0
+    tags.forEach(tag => {
+        switch (tag['name']) {
+            case 'ul':
+                level++
+                break;
+            case 'li':
+                list.push(new Paragraph({
+                    text: tag['content'],
+                    bullet: {
+                        level
+                    }
+                }))
+                break
+            case '/ul':
+                level--
+                break
+        }
+    })
+    return list
+}
